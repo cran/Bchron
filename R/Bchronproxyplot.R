7d3c128238ba04dbced79a71710d40b5d8f915f3
Andrew Parnell <Andrew.Parnell@ucd.ie>
2010-10-27 00:00:00 +0000

    version 3.1.2

--- R/Bchronproxyplot.R
+++ R/Bchronproxyplot.R
@@ -1,34 +1,24 @@
-Bchronproxyplot <-
-function(Bchrondata,proxy,title=NULL,xlabel="Age (k cal yrs BP)",ylabel="Proxy",num=10,col=c("red","red","red"),smooth=FALSE,...){
+Bchronproxyplot <- function (Bchrondata, proxy, title = NULL, xlabel = "Age (k cal yrs BP)",  ylabel = "Proxy", num = 1000, col = "blue",meancol="red", transp=0.1,...) {
 
 # Read in all chronologies  
 Bchrondata$chrons <- as.matrix(read.table(Bchrondata$chronsfile))
 Bchrondata$ranges <- read.table(Bchrondata$rangesfile,header=TRUE)
 
 # Now produce plot of lines
-xlimits <- c(min(Bchrondata$ranges[,2]),max(Bchrondata$ranges[,4]))
-ylimits <- c(min(proxy),max(proxy))
+xlimits <- c(min(Bchrondata$ranges[, 2]), max(Bchrondata$ranges[,4]))
+ylimits <- c(min(proxy), max(proxy))
 dev.new(...)
-plot(1,1,type="n",xlim=xlimits,ylim=ylimits,main=title,xlab= xlabel,ylab= ylabel,las=1)
+plot(1, 1, type = "n", xlim = xlimits, ylim = ylimits, main = title, xlab = xlabel, ylab = ylabel, las = 1)
 grid()
 
-# Plot num lines as well
-myrows <- sample(seq(1,nrow(Bchrondata$chrons)),num,replace=FALSE)
-if(num>0) for(i in 1:num) lines(Bchrondata$chrons[myrows[i],],proxy,col="grey")
+# Draw lines for all values in suitable see-through colours
+tmp <- col2rgb(col)
+mycol <- rgb(tmp[1, 1]/255, tmp[2, 1]/255, tmp[3, 1]/255)
+mycol2 <- paste(mycol, as.character(as.hexmode(round(transp*255, 0))), sep = "")
+myrows <- sample(seq(1, nrow(Bchrondata$chrons)), num, replace = FALSE)
+for(i in 1:num) lines(Bchrondata$chrons[myrows[i], ],proxy, col = mycol2)
 
-# Plot an estimate of the median line
-if(smooth==FALSE) {
-lines(Bchrondata$ranges[,2],proxy,col=col[1],lty="dotted",lwd=2)
-lines(Bchrondata$ranges[,3],proxy,col=col[2],lwd=2)
-lines(Bchrondata$ranges[,4],proxy,col=col[3],lty="dotted",lwd=2)
-} else {
-tmp <- ksmooth(Bchrondata$ranges[,2],proxy,bandwidth=bw.nrd0(Bchrondata$ranges[,2]))
-tmp2 <- ksmooth(Bchrondata$ranges[,3],proxy,bandwidth=bw.nrd0(Bchrondata$ranges[,3]))
-tmp3 <- ksmooth(Bchrondata$ranges[,4],proxy,bandwidth=bw.nrd0(Bchrondata$ranges[,4]))
-lines(tmp,col=col[1],lty="dotted",lwd=2)
-lines(tmp2,col=col[2],lwd=2)
-lines(tmp3,col=col[3],lty="dotted",lwd=2)
-}
+lines(colMeans(Bchrondata$chrons),proxy,col=meancol)
+
 
 }
-
